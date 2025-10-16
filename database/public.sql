6fc7ae66f67330f2f5203a542927c33a24cdd854
jhzhou002 <318352733@qq.com>
2025-10-16 17:20:47 +0800

    update:更新项目结构

---
+++ database/public.sql
@@ -0,0 +1,17 @@
+/*
+ Navicat Premium Data Transfer
+
+ Source Server         : opentenbase
+ Source Server Type    : PostgreSQL
+ Source Server Version : 100000
+ Source Host           : 127.0.0.1:5432
+ Source Catalog        : smart_medical
+ Source Schema         : public
+
+ Target Server Type    : PostgreSQL
+ Target Server Version : 100000
+ File Encoding         : 65001
+
+ Date: 16/10/2025 17:19:46
+*/
+
